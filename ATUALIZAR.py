bceae66b2c8e7f013cdfd609e7d6927978dd3a66
CY LUGH <125223393+CY-LUGH-TOOLS@users.noreply.github.com>
2023-03-13 02:39:01 -0300

    Add files via upload

--- ATUALIZAR.py
+++ ATUALIZAR.py
@@ -1,7 +1,7 @@
 import os
 os.system('clear')
-os.system('rm -rf REPO-MENU')
-os.system('git clone https://github.com/REPO-CYLUGH/REPO-MENU.git')
+os.system('rm -rf REPO-MENU\.')
+os.system('git clone https://github.com/REPO-CYLUGH/REPO-MENU-..git')
 os.system('clear')
 input('''Script atualizado.
 aperte enter para sair...''')
