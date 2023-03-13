6842e6490bb369f26a9aaf4edac0279165ef10a2
CY LUGH <125223393+CY-LUGH-TOOLS@users.noreply.github.com>
2023-03-13 01:16:44 -0300

    Delete config directory

--- config/genexe.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system('pip install pandas')
-os.system('pip install pywin32')
-os.system('pip install openpyxl')
-os.system('clear')
-os.system('pyinstaller --onefile IPapp.py')
--- config/installNUKE.py
+++
@@ -1,7 +0,0 @@
-import os 
-os.system('cd ..')
-os.system('git clone https://github.com/Sigma-cc/Discord-Nuke-Bot.git')
-os.system('cd Discord-Nuke-Bot')
-os.system('python3 -m pip install -r requirements.txt')
-os.system('pip install requirements.txt')
-os.system('python3 nuke-bot.py')
