ac81017686a19eb32015752b3d03a6f0771c70e5
CY LUGH <125223393+CY-LUGH-TOOLS@users.noreply.github.com>
2023-03-13 01:47:24 -0300

    Add files via upload

--- CY-LUGH3.py
+++ CY-LUGH3.py
@@ -1,3 +1,5 @@
 import subprocess, sys
 path_dir='/CY-LUGH-TOOL/REPO-MENU'
+open_dir = "open" if sys.platform == "darwin" else "xdg-open"
+subprocess.call((open_dir, path_dir))
 exec(open('CY-LUGH.py', encoding="utf-8").read(), globals())
